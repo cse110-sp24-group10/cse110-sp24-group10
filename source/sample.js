9496dfc00bddbcdf64c0ea50a55a10a9446fda4b
Anish Devineni <anishcdevineni@gmail.com>
2024-05-12 22:15:04 -0500

    Update sample.js

--- source/sample.js
+++ source/sample.js
@@ -1,5 +1,12 @@
 var unusedVar = 'I am not used';
 
+/**
+ * Takes two numbers and returns their sum.
+ * 
+ * @param {number} num1 - The first number to be added
+ * @param {number} num2 - The second number to be added
+ * @returns {number} - The sum of the two numbers.
+ */
 export function addNums(num1, num2) 
 {
     return num1 + num2;
