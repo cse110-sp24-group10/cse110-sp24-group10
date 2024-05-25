2ffb021be2dbccae9419643e0357e207232ac06b
AntDaKnee <akgonzalez@ucsd.edu>
2024-05-25 14:27:07 -0700

    Footbar fixed, new settings

--- source/journal/journal.js
+++ source/journal/journal.js
@@ -1,18 +1,25 @@
+// When click the text button, switches to the text textarea
 document.getElementById('textButton').addEventListener('click', function() {
     document.getElementById('textBox').classList.add('active');
     document.getElementById('codeBox').classList.remove('active');
+    console.log('textButton clicked');
   });
 
-  document.getElementById('codeButton').addEventListener('click', function() {
+  // When click the code button, switches to the code textarea
+document.getElementById('codeButton').addEventListener('click', function() {
     document.getElementById('codeBox').classList.add('active');
     document.getElementById('textBox').classList.remove('active');
   });
 
-  /* temporary code for moving onto previous and next journal pages */
-  document.getElementById('left-arrow').addEventListener('click', function () {
+/* temporary code for moving onto previous and next journal pages */
+document.getElementById('left-arrow').addEventListener('click', function () {
     console.log('LEFT ARROW CLICKED');
   });
 
-  document.getElementById('right-arrow').addEventListener('click', function () {
+document.getElementById('right-arrow').addEventListener('click', function () {
     console.log('RIGHT ARROW CLICKED');
+  });
+
+document.getElementById('settings').addEventListener('click', function () {
+    console.log('SETTINGS CLICKED');
   });
