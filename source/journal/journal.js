80f8d7f090267752e4e7d99ecf6bc338ce244494
Michael Chen <mic051@ucsd.edu>
2024-06-04 20:58:48 -0700

    Implement saving and loading to localstorage

--- source/journal/journal.js
+++ source/journal/journal.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let currMonth = currDate.getMonth();
     let currYear = currDate.getFullYear();
     let currNumDays = new Date(currYear, currMonth + 1, 0).getDate();
+    let currDateFormatted = currMonth + 1 + '/' + currDay + '/' + currYear;
 
     // Create a CodeMirror instance with additional features
     const editor = CodeMirror(document.getElementById('editor'), {
@@ -19,16 +20,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     function loadTags() {
-        const localStorageData = localStorage.getItem('tasks');
-        const parsedData = JSON.parse(localStorageData);
+        const localTags = localStorage.getItem('tags');
+        const parsedTags = JSON.parse(localTags);
         
         // TODO: IMPLEMENT IMPORTING ONLY TASKS THAT HAVE CURRENT DATE
         // Check if there is any data in tags
-        if (!parsedData) {
+        if (!parsedTags) {
             return;
         }
 
-        parsedData.forEach(task => {
+        parsedTasks.forEach(task => {
             const currTag = document.createElement('span');
             currTag.className = 'tag ' + task.tag; // tags are currently implemented as tag.<color>
             currTag.textContent = task.tag;
@@ -38,39 +39,61 @@ document.addEventListener('DOMContentLoaded', (event) => {
     
     // TODO: FINISH WHEN TASK LIST IS DONE ON THE LOCAL STORAGE PART
     function loadTasks() {
-        let localStorageData = localStorage.getItem('tasks');
-        let parsedData = JSON.parse(localStorageData);
+        let localTasks = localStorage.getItem('tasks');
+        let parsedTasks = JSON.parse(localTasks);
 
         // Check if there are any tasks
-        if(!parsedData) {
+        if(!parsedTasks) {
             return;
         }
 
-        parsedData.forEach(item => {
+        parsedTasks.forEach(item => {
             const currTask = document.createElement('div');
             currTask.className = 'task';
             currTask.textContet = item.task;
         });
     }
 
+    function loadTexts() {
+        const localJournal = localStorage.getItem('journal');
+        const parsedJournal = JSON.parse(localJournal);
+        
+        // Check if current day currently exists in localstorage
+        if (!parsedJournal[currDateFormatted]) {
+            return;
+        }
+
+        document.getElementById('textBox').value = parsedJournal[currDateFormatted]["textValue"];
+        editor.getDoc().setValue(parsedJournal[currDateFormatted]["editorValue"]);
+    }
 
     
     function saveToLocalStorage() {
-        let textVal = document.getElementById('textBox').value;
-        console.log("TEXTBOXVAL = " + textVal);
-        console.log("EDITORVAL = " + editor.getValue());
-
-        const currData = {
-            date: currDate.toDateString(),
-            data: {
-                textValue: document.getElementById('textBox').value,
-                editorValue: editor.getValue(),
+        /*
+        FORMAT OF JOURNAL DATA IN LOCALSTORAGE
+        journal: {
+            date1: {
+                textValue: "text1",
+                editorValue: "editor1"
             }
+            
+            date2: { 
+                textValue: "text2",
+                editorValue: "editor2"
+            }
+
+        }
+        */
+        const textVal = document.getElementById('textBox').value;
+        let allJournalData = JSON.parse(localStorage.getItem('journal') || '[]'); // get the journal array from localstorage if there is no journal array then it will create 
+        const currData = {
+            textValue: document.getElementById('textBox').value,
+            editorValue: editor.getValue(),
         };
+        
+        allJournalData[currDateFormatted] = currData; // append currData to journal array
 
-        console.log(currData);
-
-        localStorage.setItem('journal', JSON.stringify(currData));
+        localStorage.setItem('journal', JSON.stringify(allJournalData)); // save new journal object to localstorage
     }
 
     function updateDateText() {
@@ -83,33 +106,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // TODO: COMBINE LOADTAGS() AND LOADTASKS() HERE
     function populatePage() {
         const allJournalData = localStorage.getItem('journal');
-
+        
         updateDateText();
+        // TODO: default all texts before repopulating
+        document.getElementById('textBox').value = '';
+        editor.getDoc().setValue('// Enter code here');
+
         // deleting all tags and tasks before repopulating to make sure data doesn't bleed over into next pages
         document.getElementById('tags').innerHTML = '<h2>Tags:</h2>';
         document.getElementById('tasks').innerHTML = '<h2>Tasks:</h2>';
         loadTags();
         loadTasks();
-
-        
-        console.log(allJournalData);
-        // if there is theres no journal data in localstorage, initialize an empty array for it
-        if (!allJournalData) {
-            localStorage.setItem('journal', JSON.stringify([]));
-            return;
-        }
-
-        let parsedData = JSON.parse(allJournalData);
-        console.log(typeof(parsedData));
-        /*
-        parsedData.forEach(entry => {
-            if (entry.date === currDate.toDateString()) {
-                document.getElementById('textBox').value = entry.data.textValue;
-                editor.setValue(entry.data.editorValue);
-            }
-        });
-        */
-        
+        loadTexts();
     }
     populatePage();
 
@@ -139,9 +147,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
             currDay = new Date(currYear, currMonth + 1, 0).getDate();
         }
         currDate = new Date(currYear, currMonth, currDay);
+        currDateFormatted = currMonth + 1 + '/' + currDay + '/' + currYear;
+        
         updateDateText();
         populatePage();
     });
+
 
     document.getElementById('right-arrow').addEventListener('click', function () {
         // increment date by 1. check for month and year changes and adjust variables accordingly
@@ -155,6 +166,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             currDay = 1;
         }
         currDate = new Date(currYear, currMonth, currDay);
+        currDateFormatted = currMonth + 1 + '/' + currDay + '/' + currYear;
         
         updateDateText();
         populatePage();
@@ -162,5 +174,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     document.getElementById('settings').addEventListener('click', function () {
         console.log('SETTINGS CLICKED');
+        saveToLocalStorage();
     });
+
+    document.getElementById('textBox').addEventListener('keyup', saveToLocalStorage);
+
+    document.getElementById('editor').addEventListener('keyup', saveToLocalStorage);
 });
