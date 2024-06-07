f568d094f5da28c4d1bfe15e4c2e07a53af247cd
alaguelo <alaguelo@ucsd.edu>
2024-06-06 17:45:18 -0700

    jest configuration update to run unit test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   launch: {
-    headless: false,
-    slowMo: 25
+    headless: true, // Ensures Puppeteer runs in headless mode
+    args: [
+      '--no-sandbox', // Bypass the sandbox for new browser processes,
+      '--disable-setuid-sandbox', // Disable the setuid sandbox (more secure),
+      '--disable-dev-shm-usage', // Overcome limited resource problems
+      '--disable-accelerated-2d-canvas', // Disable hardware acceleration
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // Run Chromium in a single process mode
+      '--disable-gpu' // Disable GPU hardware acceleration
+    ]
+  },
+  browserContext: 'default',
+  server: {
+    command: 'node server.js', // Assuming you might have a server running for tests
+    port: 4444,
+    launchTimeout: 10000, // Timeout for launching the server
+    debug: true,
   }
-}
+};
